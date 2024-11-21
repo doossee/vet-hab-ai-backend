822f390868704c07633938fcbfcd40d7775056e6
doossee <84768497+doossee@users.noreply.github.com>
2024-11-21 12:33:42 +0500

    configured main.js file for swagger-ui rapidoc-ui and exception-filters added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,42 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, Reflector } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ClassSerializerInterceptor, ValidationPipe } from '@nestjs/common';
+import { PrismaExceptionFilter } from './common/filters/prisma-exception.filter';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { RapidocModule } from '@b8n/nestjs-rapidoc';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const port = process.env.PORT || 3000;
+  const host = process.env.HOST || '127.0.0.1';
+
+  app.enableCors();
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+  app.useGlobalFilters(new PrismaExceptionFilter());
+  app.useGlobalInterceptors(new ClassSerializerInterceptor(app.get(Reflector)));
+
+  const config = new DocumentBuilder()
+    .setTitle('dr-ikramov')
+    .setDescription('The dr-ikramov API description')
+    .setVersion('0.1')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger', app, document);
+  RapidocModule.setup('rapidoc', app, document);
+
+  await app.listen(port, host);
 }
 bootstrap();
