2ee87b84a77d7086b10ec6eacb86bfa43e80d037
doossee <84768497+doossee@users.noreply.github.com>
2024-12-05 11:24:30 +0500

    before reinstalling OS

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FarmersModule } from './farmers/farmers.module';
@@ -6,9 +6,24 @@ import { VetStationsModule } from './vet-stations/vet-stations.module';
 import { UsersModule } from './users/users.module';
 import { AuthModule } from './auth/auth.module';
 import { VeterinariansModule } from './veterinarians/veterinarians.module';
+import { ConfigModule } from '@nestjs/config';
+import { loggingMiddleware, PrismaModule } from 'nestjs-prisma';
 
 @Module({
-  imports: [FarmersModule, UsersModule, AuthModule, VeterinariansModule, VetStationsModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    PrismaModule.forRoot({
+      isGlobal: true,
+      prismaServiceOptions: {
+        middlewares: [
+          loggingMiddleware({
+            logger: new Logger('PrismaMiddleware'),
+            logLevel: 'log',
+          }),
+        ],
+      },
+    }),
+    FarmersModule, UsersModule, AuthModule, VeterinariansModule, VetStationsModule],
   controllers: [AppController],
   providers: [AppService],
 })
